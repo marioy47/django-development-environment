dfda1709a172af1c1d5b1e3cd3f0c0db84cb45e7
Mario Yepes <marioy47@gmail.com>
2021-02-02 21:38:56 -0500

    Adding GitHub actions workflow for automatic testing

---
+++ config/test.py
@@ -0,0 +1,14 @@
+# config/test.py
+from django.test import TestCase
+from django.urls import reverse
+
+
+class FirstTest(TestCase):
+    def test_that_invalid_fails(self):
+        """ This test fails on purpose """
+        self.assertTrue(False)
+
+    def test_that_testing_works(self):
+        """ This test validates that the route /test-path/ is a valid path """
+        res = self.client.get(reverse("test-path"))
+        self.assertEqual(200, res.status_code)
